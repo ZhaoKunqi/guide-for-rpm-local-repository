f7ff025f9495a6da5df97498aa1c8a8b55540048
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-09-09 13:52:20 +0800

    Create sync-el9.sh

---
+++ raw_materials/sync-el9.sh
@@ -0,0 +1,29 @@
+#alma
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/isos/x86_64/ /usr/share/nginx/el9/almalinux/9/isos/x86_64/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/BaseOS/x86_64/os/ /usr/share/nginx/el9/almalinux/9/BaseOS/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/AppStream/x86_64/os/ /usr/share/nginx/el9/almalinux/9/AppStream/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/CRB/x86_64/os/ /usr/share/nginx/el9/almalinux/9/CRB/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/HighAvailability/x86_64/os/ /usr/share/nginx/el9/almalinux/9/HighAvailability/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/ResilientStorage/x86_64/os/ /usr/share/nginx/el9/almalinux/9/ResilientStorage/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/extras/x86_64/os/ /usr/share/nginx/el9/almalinux/9/extras/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/plus/x86_64/os/ /usr/share/nginx/el9/almalinux/9/plus/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/RT/x86_64/os/ /usr/share/nginx/el9/almalinux/9/RT/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/NFV/x86_64/os/ /usr/share/nginx/el9/almalinux/9/NFV/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/SAP/x86_64/os/ /usr/share/nginx/el9/almalinux/9/SAP/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/SAPHANA/x86_64/os/ /usr/share/nginx/el9/almalinux/9/SAPHANA/x86_64/os/
+rsync -aqzH --delete rsync://mirrors.nju.edu.cn/almalinux/9/devel/x86_64/os/ /usr/share/nginx/el9/almalinux/9/devel/x86_64/os/
+
+#epel
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/epel/9/Everything/x86_64/ /usr/share/nginx/el9/epel/Everything/
+
+#rpm-fusion
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/rpmfusion/free/el/updates/9/x86_64/  /usr/share/nginx/el9/rpmfusion/free/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/rpmfusion/nonfree/el/updates/9/x86_64/ /usr/share/nginx/el9/rpmfusion/nonfree/
+
+#remi
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/9/safe/x86_64/ /usr/share/nginx/el9/remi/remi-safe/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/9/modular/x86_64/ /usr/share/nginx/el9/remi/remi-modular/
+
+#elrepo
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/elrepo/elrepo/el9/x86_64/ /usr/share/nginx/el9/elrepo/elrepo/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/elrepo/kernel/el9/x86_64/ /usr/share/nginx/el9/elrepo/kernel/
