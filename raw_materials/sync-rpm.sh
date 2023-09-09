e98269036d29d95b88591da603635005d0ab4536
Kunqi Zhao <110202952+ZhaoKunqi@users.noreply.github.com>
2023-09-09 13:53:49 +0800

    Create sync-rpm.sh

---
+++ raw_materials/sync-rpm.sh
@@ -0,0 +1,4 @@
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/kubernetes/yum/repos/kubernetes-el7-x86_64/ /usr/share/nginx/rpm/kubernetes/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/centos/9/x86_64/stable/ /usr/share/nginx/rpm/docker-ce/9/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/ceph/rpm-quincy/ /usr/share/nginx/rpm/ceph/rpm-quincy/
+rsync -aqzH --delete rsync://mirrors.tuna.tsinghua.edu.cn/ceph/rpm-reef/ /usr/share/nginx/rpm/ceph/rpm-reef/
